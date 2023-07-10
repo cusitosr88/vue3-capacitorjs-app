957416cb37b2a9468a644e007068ef0b3578836a
Javier Suarez <cusitosr88@gmail.com>
2023-07-10 19:04:51 -0400

    import new todo list component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,16 +3,7 @@
     <div class="text-h4 text-blue-1 text-weight-bold">What's up</div>
     <div class="q-pa-md">
       <add-todo/>
-      <q-list class="q-mt-lg">
-      <q-item tag="label" v-ripple class="itm-card q-my-sm">
-        <q-item-section avatar>
-          <q-radio v-model="category" val="teal" category="teal" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>Teal</q-item-label>
-        </q-item-section>
-      </q-item>
-    </q-list>
+      <todo-list/>
   </div>
   </q-page>
 </template>
@@ -20,11 +11,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import AddTodo from 'src/components/AddTodo.vue';
+import TodoList from 'src/components/TodoList.vue';
 
 export default defineComponent({
   name: 'IndexPage',
   components: {
-    AddTodo
+    AddTodo,
+    TodoList
   },
 
   setup () {
@@ -33,12 +26,3 @@ export default defineComponent({
   }
 });
 </script>
-<style lang="sass" scoped>
-.itm-card
-  background: rgba(255, 255, 255, 0.12)
-  border-radius: 16px
-  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1)
-  backdrop-filter: blur(5.2px)
-  -webkit-backdrop-filter: blur(5.2px)
-  border: 1px solid rgba(255, 255, 255, 0.23)
-</style>
